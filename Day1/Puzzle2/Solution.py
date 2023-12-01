f76d0c1f4c968b28f6f23c0bc5a14fde1d958d26
Tom Dorian <tom@helgen.tech>
2023-12-01 15:47:20 +0000

    Day1 Puzzle2

---
+++ Day1/Puzzle2/Solution.py
@@ -0,0 +1,36 @@
+file_path = "Day1/Puzzle1/Puzzle_Input.txt"
+import re
+SumofCallibration=0
+range=re.compile(f'[{0}-{9}]')
+numstrings=["one","two", "three", "four", "five", "six", "seven", "eight", "nine"
+            ,"0","1","2","3","4","5","6","7","8","9"]
+with open(file_path, 'r') as file:
+    for line in file:
+        LI=-1
+        FI=100000
+        FN=0
+        SN=0
+        FItemp=FI
+        LItemp=LI
+        Value=0
+        for num in numstrings:
+            Value=(Value+1)%10
+            try:
+                FItemp=line.index(num)
+                LItemp=line.rindex(num)
+            except:
+                pass
+            if(FItemp<FI):
+                FI=FItemp
+                FN=Value
+            if(LItemp>LI):
+                LI=LItemp
+                LN=Value
+        # print(line)
+        # print(FN)
+        # print(LN)
+        # # Wait for user input with a prompt
+        # user_input = input("Please enter something: ")
+
+        SumofCallibration=SumofCallibration+FN*10+LN
+print(SumofCallibration)
